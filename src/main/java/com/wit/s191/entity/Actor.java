eedb57dd493004a2f3e7429784161d17ca690441
Bilge <134232792+goksubilge@users.noreply.github.com>
2024-07-28 16:16:41 +0300

    add entity package

---
+++ src/main/java/com/wit/s191/entity/Actor.java
@@ -0,0 +1,42 @@
+package com.wit.s191.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name= "actor", schema = "fsweb")
+public class Actor {
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "gender")
+    private Gender gender;
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+
+    @ManyToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinTable(name="actor_movie", schema = "fsweb", joinColumns = @JoinColumn(name="actor_id"),
+    inverseJoinColumns = @JoinColumn(name="movie_id"))
+    private List<Movie> movieList;
+
+    // utility fuction
+    public void addMovie(Movie movie){
+        if(movieList == null){
+            movieList = new ArrayList<>();
+        }
+        movieList.add(movie);
+    }
+
+}
---
+++ src/main/java/com/wit/s191/entity/Gender.java
@@ -0,0 +1,6 @@
+package com.wit.s191.entity;
+
+public enum Gender {
+    MALE,
+    FEMALE
+}
